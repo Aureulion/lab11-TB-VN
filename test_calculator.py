c18f527183a77e719c0cb7e9b547c9fa32f40b8b
Veronica Nava <veronicaknava@gmail.com>
2026-04-13 15:48:38 -0400

    final changes to unit tests

--- test_calculator.py
+++ test_calculator.py
@@ -2,15 +2,7 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
-
-    ######## Partner 1
     def test_multiply(self): # 3 assertions
         self.assertEqual(multiply(2,3), 6)
         self.assertEqual(multiply(-5,10), -50)
@@ -21,29 +13,11 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(divide(-10,2), -5.0)
         self.assertAlmostEqual(divide(5,2), 2.5)
 
-    # ##########################
 
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
-
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
-
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
     
-    ######## Partner 1
     def test_log_invalid_argument(self): # 1 assertion
         with self.assertRaises(ValueError):
               logarithm(0, 5)
-
-
 
     def test_hypotenuse(self): # 3 assertions
         self.assertAlmostEqual(hypotenuse(3,4), 5.0)
@@ -57,40 +31,32 @@ class TestCalculator(unittest.TestCase):
         with self.assertRaises(ValueError):
              square_root(-4)
 
+    def test_add(self):  # 3 assertions
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
 
-# Do not touch this
-    import unittest
-    from calculator import *
+    def test_subtract(self):  # 3 assertions
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(0, 4), -4)
+        self.assertEqual(subtract(-2, -2), 0)
 
-    class TestCalculator(unittest.TestCase):
+    ##########################
 
-        ######### Partner 2
-        def test_add(self):  # 3 assertions
-            self.assertEqual(add(2, 3), 5)
-            self.assertEqual(add(-1, 1), 0)
-            self.assertEqual(add(0, 0), 0)
+    ######## Partner 2
+    def test_divide_by_zero(self):  # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            divide(0, 5)
 
-        def test_subtract(self):  # 3 assertions
-            self.assertEqual(sub(5, 3), 2)
-            self.assertEqual(sub(0, 4), -4)
-            self.assertEqual(sub(-2, -2), 0)
+    def test_logarithm(self):  # 3 assertions
+        self.assertAlmostEqual(logarithm(10, 100), 2)
+        self.assertAlmostEqual(logarithm(2, 8), 3)
+        self.assertAlmostEqual(logarithm(10, 10), 1)
 
-        ##########################
+    def test_log_invalid_base(self):  # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithm(1, 10)  # base 1 is invalid
 
-        ######## Partner 2
-        def test_divide_by_zero(self):  # 1 assertion
-            with self.assertRaises(ZeroDivisionError):
-                div(0, 5)
-
-        def test_logarithm(self):  # 3 assertions
-            self.assertAlmostEqual(log(10, 100), 2)
-            self.assertAlmostEqual(log(2, 8), 3)
-            self.assertAlmostEqual(log(10, 10), 1)
-
-        def test_log_invalid_base(self):  # 1 assertion
-            with self.assertRaises(ValueError):
-                log(1, 10)  # base 1 is invalid
-        ##########################
 
     # Do not touch this
 if __name__ == "__main__":
