e24326ec475c5da492bc67600e3f7355bc2ddc89
Tyler Bauman <Aureulion@gmail.com>
2026-04-13 15:48:39 -0400

    fix

--- calculator.py
+++ calculator.py
@@ -22,9 +22,9 @@ def add(a, b):
     return a+b
 def subtract(a,b):
     return a-b
-def multiply(a,b):
+def mul(a,b):
     return a*b
-def divide(a,b):
+def div(a,b):
     if a == 0:
         raise ZeroDivisionError()
     return b/a
@@ -32,5 +32,5 @@ def logarithm(a,b):
     if b <= 0 or a <= 1:
         raise ValueError()
     return math.log(b, a)
-def exponent(a,b):
+def exp(a,b):
     return a**b
